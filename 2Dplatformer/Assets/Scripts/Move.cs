22ff09ea1da9ed62700f69b6f0977752d69cd54e
Elessar1133 <caohs_general@hotmail.co.uk>
2018-01-16 00:01:19 +0000

    Added project to repo.

---
+++ 2Dplatformer/Assets/Scripts/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    GameObject player;
+    CharacterController Controller;
+    Rigidbody2D PlayerRB2D;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Controller = player.GetComponent<CharacterController>();
+        PlayerRB2D = player.GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+
+    public float speed = 10.0f;
+    public float jumpSpeed = 50.0f;
+    private Vector2 Movement = Vector2.zero;
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        Movement.x = Input.GetAxis("Horizontal");
+
+        PlayerRB2D.velocity = new Vector2(Movement.x * speed, 0);
+        
+        if(Input.GetButton("Jump") && grounded)
+        {
+            PlayerRB2D.velocity = new Vector2(player.transform.position.x, jumpSpeed);
+            grounded = false;
+        }
+    }
+}
---
+++ 2Dplatformer/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int playerHealth = 10;
+    GameObject player;
+    GameObject healthBar;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = 10;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
